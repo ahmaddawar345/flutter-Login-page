9d78d54be7653a837809e290e62428c1007a51b1
Ahmad <ahmaddawar345@gmail.com>
2024-11-05 05:41:14 -0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/my_gh_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_gh_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const sing_up_page());
+}
+
+class sing_up_page extends StatelessWidget {
+  const sing_up_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ,
+      ),
+    );
+  }
+}
